da64f34311171c639a86316b92369ff1a76978a5
freaky-skynet <kwonnunkal@gmail.com>
2026-05-12 17:50:42 +0900

    feat :bosspattern1 외 1. **첫번째 보스패턴 구현 @export로 boss_bullet_manager 씬에서 fire_unit 조정하거나 boss 씬의 Timer 조정하여 유기적으로 난이도 조정 가능하도록 편집함
    2. 보스<->플레이어 충돌판정 추가
    
    3. 인스펙터 CanvasItem 속성->Ordering에서 Z index 조정: 플레이어 5, 보스 0, 플레이어 총알 10, 보스총알 15
    ->게임 내에서 보이는 오브젝트의 깊이를 조정함(플레이어보다 위에 보스 총알이 보이도록)

--- boss.gd
+++ boss.gd
@@ -1,3 +1,18 @@
 extends CharacterBody2D
 
 @onready var boss_bullet_manager=%BossBulletManager
+@onready var pattern1_timer = $Timer
+var pos=Vector2.ZERO
+var variation=0.0
+
+func _on_ready() -> void:
+	pattern1_timer.start()
+	pos =global_position
+
+func _on_timer_timeout() -> void:
+	pattern1_shoot()
+
+func pattern1_shoot():
+	if(variation>20) : variation=0.0
+	boss_bullet_manager.fire_pattern1_radial(pos,variation)
+	variation+=3
