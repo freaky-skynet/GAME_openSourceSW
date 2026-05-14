20dd4b8ad1f6089d1a2ccdf8d71a7d15e917b269
freaky-skynet <kwonnunkal@gmail.com>
2026-05-14 15:20:46 +0900

    feat: comboSystem 추가, 터미널 내 출력 확인 0513 스태시 적용

--- player_bullet.gd
+++ player_bullet.gd
@@ -3,9 +3,9 @@ extends Area2D
 @export var speed: float = 500.0
 var direction: Vector2 = Vector2.ZERO
 var is_active: bool = false
+var dmg = 0#콤보레벨과 연계, on_body_entered에서 레벨에 따른 데미지 차등부여
 
 func _ready():
-	# 처음에 생성될 때는 비활성화 상태로 시작
 	deactivate()
 
 func _physics_process(delta):
@@ -18,11 +18,17 @@ func _physics_process(delta):
 	# 화면 밖으로 나가면 스스로 비활성화 (화면 크기는 프로젝트 설정을 따름)
 	var screen_size = get_viewport_rect().size
 	if position.x < -20 or position.x > screen_size.x + 20 or \
-	   position.y < -20 or position.y > screen_size.y + 20:
+		position.y < -20 or position.y > screen_size.y + 20:
 		deactivate()
-
-func _on_body_entered(_body: Node2D) -> void:
+		
+func _on_body_entered():
+	GlobalGameEvents.player_hit_enemy.emit(1)#damage 1
 	deactivate()
+'''
+bullet 개체에 level 부여
+activateL2,L3,L1에서 level 바꾸고
+func _on_body_entered()에서 if로 확인, 데미지 따라 emit
+'''
 
 # 탄환을 다시 사용할 때 호출
 func activate(pos: Vector2, dir: Vector2):
