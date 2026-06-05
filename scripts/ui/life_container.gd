184d0ae84b9808a5cea579562330a5d808680017
jungmin02 <wjdals13524@gmail.com>
2026-06-05 21:05:24 +0900

    add: 하트 포지션 변경

--- scripts/ui/life_container.gd
+++ scripts/ui/life_container.gd
@@ -1,4 +1,4 @@
-extends VBoxContainer
+extends HBoxContainer
 
 # 하트 이미지 리소스 경로
 @export var heart_texture = preload("res://Hearts.png")
@@ -20,7 +20,7 @@ func setup_hearts(max_hp: int):
 	for i in range(max_hp):
 		var heart_container = Control.new()
 		# 하트 스프라이트 크기에 맞춰서 방 크기 지정
-		heart_container.custom_minimum_size = Vector2(20, 20)
+		heart_container.custom_minimum_size = Vector2(25, 25)
 		
 		# 실제 애니메이션을 돌릴 Sprite2D 생성
 		var heart_sprite = Sprite2D.new()
@@ -31,6 +31,7 @@ func setup_hearts(max_hp: int):
 		heart_sprite.vframes = 7
 		heart_sprite.centered = false
 		heart_sprite.position = Vector2(0, 0)
+		heart_sprite.scale=Vector2(1.5,1.5)
 		
 		heart_container.add_child(heart_sprite)
 		add_child(heart_container)
